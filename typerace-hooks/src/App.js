ea411257221719ca9a78cebf5d630f403112d1c1
Kensen <hirohtk@gmail.com>
2020-02-27 20:41:04 -0800

    initial commit- following https://medium.com/@dtkatz/react-hooks-tutorial-learn-by-building-b90ec4db2b8e

---
+++ typerace-hooks/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h2>Type Race</h2>
+      <input></input>
+    </div>
+  )
+}
+
+export default App;

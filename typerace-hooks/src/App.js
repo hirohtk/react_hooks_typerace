7308c05ed42d35a291b2ae122430117603a9f0b2
Kensen <hirohtk@gmail.com>
2020-03-01 20:33:39 -0800

    finished tutorial.  reinforced expressions when used with .map, learned about alternative to moment package, and learned briefly about useEffect.  next step to start timer only when user starts typing

--- typerace-hooks/src/App.js
+++ typerace-hooks/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -24,32 +24,55 @@ const App = () => {
 
   const [userText, setUserText] = useState("");
 
+  const INITIAL_GAME_STATE = { victory: false, startTime: null, endTime: null};
+
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+
   const updateUserText = event => {
     setUserText(event.target.value);
     console.log(`current userText is ${userText}`);
+    console.log(`versus event target value which is ${event.target.value}`);
+    console.log(`snippet is ${snippet}`)
+    // need to do this with event.target.value, not userText
+    if (event.target.value === snippet) {
+      let finishTime = new Date().getTime();
+      let totalTime = finishTime - gameState.startTime;
+      setGameState({...gameState, victory: true, endTime: finishTime, totalTime: totalTime})
+      console.log(`it took you ${totalTime} to win this game`)
+    }
   }
 
   const chooseSnippet = snippetIndex => {
     console.log('setSnippet', snippetIndex);
     setSnippet(SNIPPETS[snippetIndex]);
+    // Once snippet is chosen, begin the game, which includes everything in the current gamestate,/
+    // but now, changing the startTime to now
+    setGameState({ ...gameState, startTime: new Date().getTime()})
+    document.getElementById("textbox").focus();
   };
+
+  // THIS IS HOW YOU DO ASYNCHRONOUS THINGS- useEffect fires at the refresh of component
+  useEffect(() => {
+    console.log(`useEffect fired.  gameState with new Time is now ${gameState.startTime}`);
+  }, [gameState])
 
   return (
     <div>
       <h2>Type Race</h2>
       {snippet}
-      <input value={userText} onChange={updateUserText}></input>
+      <input id="textbox" value={userText} onChange={updateUserText}></input>
 
-      {SNIPPETS.map( (each, index) => {
+      {SNIPPETS.map((each, index) => (
         // NEED callback to call chooseSnippet
         <button onClick={() => chooseSnippet(index)} key={index}>
           {/* key Is used during .map so that you can have unique identifiers for 
       all of that which you are mapping.  Index comes inherently with array */}
           {each}
         </button>
-      })}
+      ))
+      }
 
-      
+      {gameState.victory ? <h1>Game finished in {gameState.totalTime} milliseconds</h1> : ""}
 
       {/* 
 

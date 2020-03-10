76164647fc33f176bd0e37c302d93bf144050af7
Kensen <hirohtk@gmail.com>
2020-03-09 22:29:27 -0700

    route built for posting where score associates with quote, but need to figure out why quote is not getting the scores in db.  may need to find the right association for quotes to scores

--- routes/all.js
+++ routes/all.js
@@ -26,39 +26,50 @@ router.get("/scrape", function (req, res) {
 });
 
 router.get("/api/:quote", (req, res) => {
+  console.log("get route firing")
   db.Quotes.findOne({quote: req.params.quote}).populate("Scores")
   .then(response => {
+    if (response != null) {
       console.log(response)
       // middleware here to sort for high scores
       res.json(response);
+    }
+    else {
+      res.json("no scores yet on this quote!")
+    }
+      
   })// if there is no quote in the db yet, that means no one has submitted a high score.  db will return an error, I believe
   .catch(err => res.json(err));
 });
 
-router.post("/api/:quote", (req, res) => {
+router.post("/api/quote", (req, res) => {
 
-  let poster = () => {
+  let poster = (quoteId) => {
     let obj = {
-      quote: req.params.quote,
+      quote: quoteId,
       name: req.body.name,
       score: req.body.score,
     }
     db.Scores.create(obj).then((response) => {
-      console.log(response);
       res.json(response);
     })
   }
 
-  // if there already exists this quote in the db, post your score
-  db.Quotes.findOne({quote: req.params.quote}).then(() => {
-    poster();
-  // but if no quote is found, then create the quote, then post your score 
-  }).catch(err => {
-    db.Quotes.create({quote: req.params.quote}).then(() => {
-    poster();
-    })
-  })
-  
+  db.Quotes.findOne({quote: req.body.quote}).then(response => {
+    // if there already exists this quote in the db, post your score
+    if (response != null) {
+      console.log("FOUND QUOTE")
+      console.log(`quote id is ${response._id}.  I'm looking for object ID of this quote`)
+      poster(response._id);
+    }
+    // but if no quote is found, then create the quote, then post your score 
+    else {
+    console.log("COULD NOT FIND QUOTE")
+    db.Quotes.create({quote: req.body.quote}).then((response) => {
+    console.log(`quote id is ${response._id}.  I'm looking for object ID of this quote`)
+      poster(response._id);})
+    }
+  }).catch(err => res.json(err))
 });
 
 // If no API routes are hit, send the React app
